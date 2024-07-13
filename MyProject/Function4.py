ba3d9d82b2d1666538e306089db514b844e9282b
bakriHFB <ahmed.803700@gmail.com>
2024-07-13 23:43:38 +0300

    Function4_improvment_commit

--- MyProject/Function4.py
+++ MyProject/Function4.py
@@ -3,4 +3,11 @@ def showShapesAreaFormula(shape):
         print(" πr²")
     elif shape == "triangle":
         print("(1/2) * Base * Height")
+    elif shape == "Square":
+        print("side × side")
+    elif shape == "parallelogram":
+        print("Base × Height")
+    elif shape == "trapezoid":
+        print("1/2 * (Base1 + Base2) * Height")
+    
        
--- MyProject/main.py
+++ MyProject/main.py
@@ -5,6 +5,5 @@ def runApp():
 
 
 
-
 if __name__ == "__main__":
     runApp()
